456f67d6dec94f71d46d1520e394da1e312071d6
seen-been <sean.heathcote@hotmail.co.uk>
2022-10-05 18:12:17 +0100

    initial commit

---
+++ CalcMain.cs
@@ -0,0 +1,64 @@
+﻿using CalculatorProj;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Calculator calc = new();
+        bool choice = true;
+
+
+        while (choice)
+        {
+            //Operation select
+            Console.WriteLine("Please enter a number listed to select an operation");
+            Console.WriteLine("1: Addition");
+            Console.WriteLine("2: Subtraction");
+            Console.WriteLine("3: Multiplication");
+            Console.WriteLine("4: Division");
+            Console.WriteLine("5: Percentage Calculation");
+            double input = Convert.ToDouble(Console.ReadLine());
+
+            switch(input)
+            {
+                case 1:
+                    Console.WriteLine("You have selected ADDITION");
+                    Console.WriteLine("The result is " + Calculator.Add());
+                    break;
+               case 2:
+                    Console.WriteLine("You have selected SUBTRACTION");
+                    Console.WriteLine("The result is " + Calculator.Subtract());
+                    break;
+                case 3:
+                    Console.WriteLine("You have selected DIVISION");
+                    Console.WriteLine("The result is " + Calculator.Divide());
+                    break;
+                case 4:
+                    Console.WriteLine("You have selected MULTIPLICATION");
+                    Console.WriteLine("The result is " + Calculator.Multiply());
+                    break;
+                case 5:
+                    Console.WriteLine("You have selected PERCENTAGE CALC");
+                    Console.WriteLine("The result is " + Calculator.Percentage());
+                    break;
+                default:
+                    Console.WriteLine("Option chosen is not a valid input.");
+                    break;
+            }
+
+            Console.WriteLine("Would you like to retry?");
+            Console.WriteLine("1: Yes");
+            Console.WriteLine("2: No");
+
+            int retry = Convert.ToInt32(Console.ReadLine());
+
+            if (retry == 1)
+                continue;
+            else
+                Console.WriteLine("Exiting...");
+                choice = false;
+        }
+
+
+    }
+}
---
+++ Calculator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorProj
+{
+    internal class Calculator
+    {
+        
+
+        public static double Add()
+        {
+            Console.WriteLine("Please enter a first number:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please enter a second number:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            return num1 + num2;
+        }
+
+        public static double Subtract()
+        {
+            Console.WriteLine("Please enter a first number:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please enter a second number:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            return num1 - num2;
+        }
+        public static double Divide()
+        {
+            Console.WriteLine("Please enter a first number:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please enter a second number:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            return num1 / num2;
+        }
+        public static double Multiply()
+        {
+            Console.WriteLine("Please enter a first number:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please enter a second number:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            return num1 * num2;
+        }
+        public static double Percentage()
+        {
+            Console.WriteLine("Please enter a first number:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please enter a second number:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            return num1 / num2 * 100;
+        }
+    }
+}
